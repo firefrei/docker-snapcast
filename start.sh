7ca4e7cc074c464fe1988ea2a2f9614151bfe706
Matthias Frei <mf@frei.media>
2021-12-28 12:58:28 +0100

    Add creation of self-signed certificates

--- start.sh
+++ start.sh
@@ -9,7 +9,17 @@ dbus-daemon --system
 # Start avahi
 avahi-daemon  --daemonize --no-chroot
 
-# Start NGINX
+# NGINX: generate self-signed ssl certificates, if no certs are existant
+if [ -s /srv/certs/snapserver.pem ] || [ -s /srv/certs/snapserver.key ] || [ -n "${SKIP_SSL_GENERATE}" ]; then
+    echo "Server SSL certificates for NGINX already exist, skipping generation."
+else
+    echo "Generating self-signed certificates for NGINX."
+    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
+        -keyout /srv/certs/snapserver.pem -out /srv/certs/snapserver.key \
+        -subj "/C=DE/ST=Bavaria/L=Nuremberg/O=Snapserver/CN=snapserver"
+fi
+
+# NGINX: Start
 nginx
 
 # Start snapserver (will start shairpot-sync automatically)
